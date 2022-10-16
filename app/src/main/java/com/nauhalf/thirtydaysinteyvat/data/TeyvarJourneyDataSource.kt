884f8f938b5467b5821951d1519bfc69b84ae50a
Naufal <m.naufal.fadhillah98@gmail.com>
2022-10-16 14:23:06 +0700

    add string resource & dummy list

---
+++ app/src/main/java/com/nauhalf/thirtydaysinteyvat/data/TeyvarJourneyDataSource.kt
@@ -0,0 +1,158 @@
+package com.nauhalf.thirtydaysinteyvat.data
+
+import com.nauhalf.thirtydaysinteyvat.R
+import com.nauhalf.thirtydaysinteyvat.model.TeyvatJourney
+
+object TeyvarJourneyDataSource {
+    val list = listOf(
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_1,
+            titleRes = R.string.title_1,
+            descriptionRes = R.string.description_1,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_2,
+            titleRes = R.string.title_2,
+            descriptionRes = R.string.description_2,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_3,
+            titleRes = R.string.title_3,
+            descriptionRes = R.string.description_3,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_4,
+            titleRes = R.string.title_4,
+            descriptionRes = R.string.description_4,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_5,
+            titleRes = R.string.title_5,
+            descriptionRes = R.string.description_5,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_6,
+            titleRes = R.string.title_6,
+            descriptionRes = R.string.description_6,
+        ),TeyvatJourney(
+            imageRes = R.drawable.teyvat_7,
+            titleRes = R.string.title_7,
+            descriptionRes = R.string.description_7,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_8,
+            titleRes = R.string.title_8,
+            descriptionRes = R.string.description_8,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_9,
+            titleRes = R.string.title_9,
+            descriptionRes = R.string.description_9,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_10,
+            titleRes = R.string.title_10,
+            descriptionRes = R.string.description_10,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_11,
+            titleRes = R.string.title_11,
+            descriptionRes = R.string.description_11,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_12,
+            titleRes = R.string.title_12,
+            descriptionRes = R.string.description_12,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_13,
+            titleRes = R.string.title_13,
+            descriptionRes = R.string.description_13,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_4,
+            titleRes = R.string.title_14,
+            descriptionRes = R.string.description_14,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_15,
+            titleRes = R.string.title_15,
+            descriptionRes = R.string.description_15,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_16,
+            titleRes = R.string.title_16,
+            descriptionRes = R.string.description_16,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_17,
+            titleRes = R.string.title_17,
+            descriptionRes = R.string.description_17,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_18,
+            titleRes = R.string.title_18,
+            descriptionRes = R.string.description_18,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_19,
+            titleRes = R.string.title_19,
+            descriptionRes = R.string.description_19,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_20,
+            titleRes = R.string.title_20,
+            descriptionRes = R.string.description_20,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_21,
+            titleRes = R.string.title_21,
+            descriptionRes = R.string.description_21,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_22,
+            titleRes = R.string.title_22,
+            descriptionRes = R.string.description_22,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_23,
+            titleRes = R.string.title_23,
+            descriptionRes = R.string.description_23,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_24,
+            titleRes = R.string.title_24,
+            descriptionRes = R.string.description_24,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_25,
+            titleRes = R.string.title_25,
+            descriptionRes = R.string.description_25,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_26,
+            titleRes = R.string.title_26,
+            descriptionRes = R.string.description_26,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_27,
+            titleRes = R.string.title_27,
+            descriptionRes = R.string.description_27,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_28,
+            titleRes = R.string.title_28,
+            descriptionRes = R.string.description_28,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_29,
+            titleRes = R.string.title_29,
+            descriptionRes = R.string.description_29,
+        ),
+        TeyvatJourney(
+            imageRes = R.drawable.teyvat_30,
+            titleRes = R.string.title_30,
+            descriptionRes = R.string.description_30,
+        ),
+    )
+}
